6f6d75835f2c15caea186c9e09621c424e064515
andrear23 <andrear23@skel.ru.is>
2023-09-13 21:04:07 +0000

    Questions 1 & 2

--- problem4/process.c
+++ problem4/process.c
@@ -2,6 +2,14 @@
 
 void processMessage(unsigned int data)
 {
-    unsigned int letter = data;
-    printLetter(letter);
+    const unsigned int letter = data;
+    const unsigned int a = (0xFF000000 & letter) >> (3 * 8);
+    const unsigned int b = (0x00FF0000 & letter) >> (2 * 8);
+    const unsigned int c = (0x0000FF00 & letter) >> (1 * 8);
+    const unsigned int d = (0x000000FF & letter) >> (0 * 8);
+
+    printLetter(a);
+    printLetter(b);
+    printLetter(c);
+    printLetter(d);
 }
